8f7f30bfd86a3b46540c2fee4214d5af7f33fd34
animus apertus <37455165+ethantatum@users.noreply.github.com>
2018-11-29 07:04:33 -0700

    added questionbank object with placeholder text, font awesone flame icon to style

---
+++ assets/javascript/app.js
@@ -0,0 +1,128 @@
+$(document).ready(function() {
+    // VARIABLES
+    // ==================================================================================
+    const questionBank = {
+        question1: {
+            question: `placeholder`,
+            answer1: `placeholder`,
+            answer2: `placeholder`,
+            answer3: `placeholder`,
+            answer4: `placeholder`,
+            picture: `placeholder.jpg`
+        },
+        question2: {
+            question: `placeholder`,
+            answer1: `placeholder`,
+            answer2: `placeholder`,
+            answer3: `placeholder`,
+            answer4: `placeholder`,
+            picture: `placeholder.jpg`
+        },
+        question3: {
+            question: `placeholder`,
+            answer1: `placeholder`,
+            answer2: `placeholder`,
+            answer3: `placeholder`,
+            answer4: `placeholder`,
+            picture: `placeholder.jpg`
+        },
+        question4: {
+            question: `placeholder`,
+            answer1: `placeholder`,
+            answer2: `placeholder`,
+            answer3: `placeholder`,
+            answer4: `placeholder`,
+            picture: `placeholder.jpg`
+        },
+        question5: {
+            question: `placeholder`,
+            answer1: `placeholder`,
+            answer2: `placeholder`,
+            answer3: `placeholder`,
+            answer4: `placeholder`,
+            picture: `placeholder.jpg`
+        },
+        question6: {
+            question: `placeholder`,
+            answer1: `placeholder`,
+            answer2: `placeholder`,
+            answer3: `placeholder`,
+            answer4: `placeholder`,
+            picture: `placeholder.jpg`
+        },
+        question7: {
+            question: `placeholder`,
+            answer1: `placeholder`,
+            answer2: `placeholder`,
+            answer3: `placeholder`,
+            answer4: `placeholder`,
+            picture: `placeholder.jpg`
+        },
+        question8: {
+            question: `placeholder`,
+            answer1: `placeholder`,
+            answer2: `placeholder`,
+            answer3: `placeholder`,
+            answer4: `placeholder`,
+            picture: `placeholder.jpg`
+        },
+        question9: {
+            question: `placeholder`,
+            answer1: `placeholder`,
+            answer2: `placeholder`,
+            answer3: `placeholder`,
+            answer4: `placeholder`,
+            picture: `placeholder.jpg`
+        },
+        question10: {
+            question: `placeholder`,
+            answer1: `placeholder`,
+            answer2: `placeholder`,
+            answer3: `placeholder`,
+            answer4: `placeholder`,
+            picture: `placeholder.jpg`
+        },
+    },
+
+
+
+    // FUNCTIONS
+    // ==================================================================================
+
+    const thirtySeconds = setTimeout(function() {
+
+    }, 30000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
