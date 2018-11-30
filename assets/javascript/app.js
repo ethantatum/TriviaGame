d672c1140e57530962c429fcfd10a6a8fc23268b
animus apertus <37455165+ethantatum@users.noreply.github.com>
2018-11-30 11:28:31 -0700

    added images, questions, and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,88 +1,118 @@
 $(document).ready(function() {
     // VARIABLES
     // ==================================================================================
-    const questionBank = {
-        question1: {
-            question: `placeholder`,
-            answer1: `placeholder`,
-            answer2: `placeholder`,
-            answer3: `placeholder`,
-            answer4: `placeholder`,
-            picture: `placeholder.jpg`
+    const questionBank = [
+        {
+            question: `Which U.S. president was in office for the majority of the Civil War?`,
+            answers: {
+            a: `Ulysses S. Grant`,
+            b: `Abraham Lincoln`,
+            c: `Ronald Reagan`,
+            d: `Thomas Jefferson`
+            },
+            correctAnswer: `b`,
+            picture: `../images/civil-war.jpg`
         },
-        question2: {
-            question: `placeholder`,
-            answer1: `placeholder`,
-            answer2: `placeholder`,
-            answer3: `placeholder`,
-            answer4: `placeholder`,
-            picture: `placeholder.jpg`
+        {
+            question: `Which leader, shown below, advocated non-violent resistance to the British presence in India?`,
+            answers: {
+            a: `Aziz Ansari`,
+            b: `Jawaharlal Nehru`,
+            c: `Mahatma Gandhi`,
+            d: `Indira Gandhi`
+            },
+            correctAnswer: `c`,
+            picture: `../images/indian-leader.jpg`
         },
-        question3: {
-            question: `placeholder`,
-            answer1: `placeholder`,
-            answer2: `placeholder`,
-            answer3: `placeholder`,
-            answer4: `placeholder`,
-            picture: `placeholder.jpg`
+        {
+            question: `In 1964, this band famously appeared on the Ed Sullivan Show for the first time.`,
+            answers: {
+            a: `The Rolling Stones`,
+            b: `The Who`,
+            c: `NSYNC`,
+            d: `The Beatles`
+            },
+            correctAnswer: `d`,
+            picture: `../images/ed-sullivan.jpg`
         },
-        question4: {
-            question: `placeholder`,
-            answer1: `placeholder`,
-            answer2: `placeholder`,
-            answer3: `placeholder`,
-            answer4: `placeholder`,
-            picture: `placeholder.jpg`
+        {
+            question: `After spending 27 years in prison, Nelson Mandela was released in 1990, and went on to be elected president of which country?`,
+            answers: {
+            a: `South Africa`,
+            b: `Kenya`,
+            c: `Ethiopia`,
+            d: `Canada`
+            },
+            correctAnswer: `a`,
+            picture: `../images/freedom.jpg`
         },
-        question5: {
-            question: `placeholder`,
-            answer1: `placeholder`,
-            answer2: `placeholder`,
-            answer3: `placeholder`,
-            answer4: `placeholder`,
-            picture: `placeholder.jpg`
+        {
+            question: `The 2000 U.S. presidential election was plagued by issues with ballots in which U.S. state?`,
+            answers: {
+            a: `Cuba`,
+            b: `Texas`,
+            c: `Florida`,
+            d: `Minnesota`
+            },
+            correctAnswer: `c`,
+            picture: `../images/hanging-chad.jpg`
         },
-        question6: {
-            question: `placeholder`,
-            answer1: `placeholder`,
-            answer2: `placeholder`,
-            answer3: `placeholder`,
-            answer4: `placeholder`,
-            picture: `placeholder.jpg`
+        {
+            question: `At a press conference in 1991, this famous NBA player announced he had tested positive for HIV.`,
+            answers: {
+            a: `Michael Jordan`,
+            b: `Lebron James`,
+            c: `Magic Johnson`,
+            d: `Dominique Wilkins`
+            },
+            correctAnswer: `c`,
+            picture: `../images/announcement.jpg`
         },
-        question7: {
-            question: `placeholder`,
-            answer1: `placeholder`,
-            answer2: `placeholder`,
-            answer3: `placeholder`,
-            answer4: `placeholder`,
-            picture: `placeholder.jpg`
+        {
+            question: `Who is nightclub owner Jack Ruby (back to the camera) seen firing at in the image below?`,
+            answers: {
+            a: `Lee Oswald`,
+            b: `Lyndon Johnson`,
+            c: `Mark David Chapman`,
+            d: `Charlie Chaplin`
+            },
+            correctAnswer: `a`,
+            picture: `../images/jack-ruby.jpg`
         },
-        question8: {
-            question: `placeholder`,
-            answer1: `placeholder`,
-            answer2: `placeholder`,
-            answer3: `placeholder`,
-            answer4: `placeholder`,
-            picture: `placeholder.jpg`
+        {
+            question: `Near the end of World War II, Winston Churchill, Franklin Delano Roosevelt, and Joseph Stalin met to discuss post-war issues at the __________.`,
+            answers: {
+            a: `Paris Agreement`,
+            b: `Potsdam Conference`,
+            c: `Warsaw Pact`,
+            d: `Yalta Conference`
+            },
+            correctAnswer: `d`,
+            picture: `../images/big-three.jpg`
         },
-        question9: {
-            question: `placeholder`,
-            answer1: `placeholder`,
-            answer2: `placeholder`,
-            answer3: `placeholder`,
-            answer4: `placeholder`,
-            picture: `placeholder.jpg`
+        {
+            question: `Which African dicator was in power for 42 years, until his overthrow, capture, and death in 2011?`,
+            answers: {
+            a: `Hosni Mubarak`,
+            b: `Muammar Gaddafi`,
+            c: `Barack Obama`,
+            d: `Idi Amin`
+            },
+            correctAnswer: `b`,
+            picture: `../images/sunglasses.jpg`
         },
-        question10: {
-            question: `placeholder`,
-            answer1: `placeholder`,
-            answer2: `placeholder`,
-            answer3: `placeholder`,
-            answer4: `placeholder`,
-            picture: `placeholder.jpg`
+        {
+            question: `The military response to protests in which Asian country led to the iconic photograph shown below?`,
+            answers: {
+            a: `China`,
+            b: `Russia`,
+            c: `Tokyo`,
+            d: `Myanmar`
+            },
+            correctAnswer: `a`,
+            picture: `../images/tank-man.jpg`
         },
-    },
+    ];
 
 
 
